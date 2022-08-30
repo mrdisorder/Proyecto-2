842698e060c486370c90f1c43569a32fca3b4396
Aldoyvh <zap333@amerike.edu.mx>
2022-08-30 07:33:17 -0500

    You can now enter a sequence and show it on console pressing enter

--- Proyecto2/Assets/scripts/SecuenciaIngresada.cs
+++ Proyecto2/Assets/scripts/SecuenciaIngresada.cs
@@ -4,26 +4,80 @@ using UnityEngine;
 
 public class SecuenciaIngresada : MonoBehaviour
 {
-    public List<Steps> secuenciaIngresada;
+    [SerializeField] int maxNumbers = 100;
+    private List<Steps> secuenciaIngresada;
 
     // Start is called before the first frame update
     void Start()
     {
         secuenciaIngresada = new List<Steps>();
-        IngresarSecuencia();
+        
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            secuenciaIngresada.Add(Steps.left);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            secuenciaIngresada.Add(Steps.right);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            secuenciaIngresada.Add(Steps.left);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            secuenciaIngresada.Add(Steps.left);
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            foreach (Steps data in secuenciaIngresada)
+            {
+                Debug.Log(data.ToString());
+            }
+        }
+
+        
+    }
+
+    /*
     public void IngresarSecuencia()
     {
+    
 
-    }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                
+                secuenciaIngresada.Add(Steps.left);
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                Steps paso = (Steps)2;
+                secuenciaIngresada.Add(paso);
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Steps paso = (Steps)3;
+                secuenciaIngresada.Add(paso);
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Steps paso = (Steps)4;
+                secuenciaIngresada.Add(paso);
+            }
 
-    // Update is called once per frame
-    void Update()
-    {
+        
+
         foreach (Steps data in secuenciaIngresada)
         {
             Debug.Log(data.ToString());
         }
     }
+     */
+
+
 }
